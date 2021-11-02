85fb87e6790d65d9ddc4a443ff02ca1cc9ac7048
nsdafhbfuigba/nfdiosajfainsdfjkbefiojk;fsamjksdfaiohfsdas <78903358+vidhanwhjr@users.noreply.github.com>
2021-11-02 16:23:04 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,34 @@
+noseX = "";
+noseY = "";
 function preload(){
 
 }
 function setup(){
     canvas = createCanvas(300, 300);
-    canvas.center();
+    canvas.position(600, 250);
     img = createCapture(VIDEO);
     img.hide();
+
+    posenet = ml5.poseNet(img, modelLoaded);
+    posenet.on('pose', gotPoses);
 }
 function draw(){
-
+   image(img, 0, 0, 300, 300);
 }
 
 function download(){
     save('mustachefilter.png');
 }
+
+function modelLoaded(){
+    console.log("MODEL LOADED");
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        noseX = results[0].pose.nose.X;
+        noseY = results[0].pose.nose.Y;
+    }
+}
+
